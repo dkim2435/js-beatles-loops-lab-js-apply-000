9b7b0d8a5ba7dd565e4935db12c5dba870a3eee1
Evan Kim <dkim2435@gmail.com>
2019-04-10 18:56:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 function theBeatlesPlay(musicians, instruments) {
   const arr = [];
   for (let i=0;i<musicians.length;i++) {
-    arr.push(${musicians[i]} plays ${instruments[i]});
-    
+    arr.push('${musicians[i]} plays ${instruments[i]}');
   }
+  return arr;
 }
+
+function johnLennonFacts
